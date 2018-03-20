787a0b41aa1ce19519dd486314125d7f0108a6e8
anumehaagrawal <anuzenith29@gmail.com>
2018-03-20 17:57:31 +0530

    Model for college

--- student/models.py
+++ student/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class College(models.Model):
+	title=models.TextField()
+	reviews=models.TextField()
+	fees=models.IntegerField()
+	exam=models.CharField(max_length=255)
+	score=models.IntegerField()
+	specialisation=models.TextField()
+	location=models.TextField()
