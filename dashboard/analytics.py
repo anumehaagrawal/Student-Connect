0e6166defe861e6deeafdb491b00e303fdf4e793
anumehaagrawal <anuzenith29@gmail.com>
2018-04-06 14:58:38 +0530

    bing api

--- dashboard/analytics.py
+++ dashboard/analytics.py
@@ -5,7 +5,10 @@ from .models import College
 from nltk.corpus import wordnet
 from django.db import models
 from itertools import product
-
+import urllib.parse,httplib2
+import json
+subscribe_entity="70d88b730e4944429c03697f8d4587f4"
+subscription_key_img = "25fcd5bc41aa4995998304649f8646f7"
 subscription_key="a0cdcb30c0894f918d92c0de8f34d85e"
 assert subscription_key
 
@@ -57,3 +60,57 @@ def recommend_college(student_income,student_interest,student_ethnic,student_sco
 					if(student_income*4*152/100 > int(col.fees)):
 						college_suggest.append(col.title)
 	return college_suggest
+
+def image_search(subscription_key,interests,college):   
+    client = ImageSearchAPI(CognitiveServicesCredentials(subscription_key_img))
+    image_results = client.images.search(query="banner"+ college)
+    if image_results.value:
+        first_image_result = image_results.value[0]
+        url=first_image_result.thumbnail_url
+        return url
+
+    else:
+        return None
+    
+def get_suggestions (query):
+    host = 'api.cognitive.microsoft.com'
+    path = '/bing/v7.0/entities'
+    mkt = 'en-US'
+    params = '?mkt=' + mkt + '&q=' + urllib.parse.quote (query)
+    headers = {'Ocp-Apim-Subscription-Key': subscribe_entity}
+    conn = httplib2.Http()
+    result=conn.request ("https://api.cognitive.microsoft.com/"+path+params,'GET',headers=headers)[1]
+    result=result.decode('utf8')
+    #response = conn.getresponse ()
+    #result=response.read ()
+    json_array=json.dumps(json.loads(result), indent=4)
+    json_obj=json.loads(json_array)
+    print()
+    result={}
+    for key in json_obj:
+        if key=="queryContext":
+            result["name"]=json_obj[key]["originalQuery"]
+        if key=="entities":
+            res=json_obj[key]["value"]
+            for i in res:
+                key=i.keys()
+                if "contractualRules" in key :
+                    contractVal=i["contractualRules"][1]
+                    if contractVal["text"]=="Wikipedia":
+                        result["wikiurl"]=contractVal["url"]
+                if "image" in key:
+                    imageVal=i["image"]
+                    imageKey=imageVal.keys()
+                    if "thumbnailUrl" in imageKey:
+                        result["image"]=imageVal["thumbnailUrl"]
+                    if "hostPageUrl" in imageKey:
+                        result["logo"]=imageVal["hostPageUrl"]
+                    if "provider" in imageKey:
+                        provVal=imageVal["provider"]
+                        for p in provVal:
+                            keyp=p.keys()
+                            if "url" in keyp:
+                                result["web"]=p["url"]
+                if "description" in key:
+                    result["desciption"]=i["description"]
+    return result
