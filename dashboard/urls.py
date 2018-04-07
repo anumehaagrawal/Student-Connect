d8b023fcf32cfa198857b804a72c4ff3f2fe1f7b
anumehaagrawal <anuzenith29@gmail.com>
2018-04-07 15:27:48 +0530

    college view

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from .views import home, colleges, recommendations, counsellors, profile, counsellors_profile, college_profile,chat,token
+from .views import home, colleges, recommendations, counsellors, profile, counsellors_profile, college_profile
 
 urlpatterns = [
 	url(r'^$', home, name='home'),
@@ -7,9 +7,8 @@ urlpatterns = [
 	url(r'^recommendations/$',recommendations),
 	url(r'^counsellors/$', counsellors),
 	url(r'^profile/', profile),
-	url(r'^chat/',chat),
 	url(r'^counsellors_profile/(?P<counsellor_username>[\w\-]+)/$', counsellors_profile, name='counsellors_profile'),
 	url(r'^college_profile/(?P<number>\d+)/$', college_profile, name='college_profile'),
-	url(r'token$', token),
+	
 
 ]
